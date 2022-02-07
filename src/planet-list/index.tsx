20de27c6bca0c711e320465a51b39e83d47466c4
akachicon <akachicon@gmail.com>
2022-02-07 10:52:31 +0300

    wip

---
+++ src/planet-list/index.tsx
@@ -0,0 +1,7 @@
+import {PlanetListQuery} from './__generated__/planet-list.query';
+
+declare const planetList: PlanetListQuery;
+
+planetList?.allPlanets?.edges?.forEach((edge) => {
+  console.log(edge?.node?.id, edge?.node?.name);
+});
