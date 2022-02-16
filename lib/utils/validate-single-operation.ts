dfa13c071067282b950040d6a4630924f063e6d1
Denis Zharenko <d.zharenko@spellsystems.com>
2022-02-16 19:21:14 +0300

    Add single operation validation for a document

--- lib/utils/validate-single-operation.ts
+++ lib/utils/validate-single-operation.ts
@@ -1,11 +1,40 @@
+import { Kind, visit } from 'graphql';
+import { formatMessageFactory } from './formatting';
+
+const PACKAGE_NAME = 'validate-single-operation';
+const formatMessage = formatMessageFactory(PACKAGE_NAME);
+
+import type { ASTVisitor } from 'graphql';
 import type { Types } from '@graphql-codegen/plugin-helpers';
 
 export function validateSingleOperation(document: Types.DocumentFile): {
   isValid: boolean;
   hasOperation: boolean;
 } {
+  if (!document.document) {
+    throw new Error(
+      formatMessage(
+        'undefined document cannot be validated:\n' +
+          `\tlocation=${document.location}\n` +
+          `\tSDL=${document.rawSDL}`,
+      ),
+    );
+  }
+  let operationCount = 0;
+  const visitor: ASTVisitor = {
+    [Kind.OPERATION_DEFINITION]: {
+      enter() {
+        operationCount += 1;
+
+        // Skip subtree to improve performance.
+        return false;
+      },
+    },
+  };
+  visit(document.document, visitor);
+
   return {
-    isValid: true,
-    hasOperation: true,
+    isValid: operationCount <= 1,
+    hasOperation: operationCount >= 1,
   };
 }
